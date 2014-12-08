1f449a3115063d7ec8cfb01c931946ab2b88ce6a
Salas Daniel <adm-2w22a-06.univ-avignon.fr>
2014-12-08 16:36:43 +0100

    Enjolivage de l'affichage des messages.

--- supAIR/view/message.php
+++ supAIR/view/message.php
@@ -1,9 +1,10 @@
 <?php
 
-	
-	echo $message->m_parent."<br>";
-	echo $message->m_emetteur."<br>";
-	echo $message->m_destinataire."<br>";
-	echo $message->m_post."<br>";
+	if($message->m_parent!=$message->m_emetteur)
+		echo "Posté par: ".$message->m_parent->identifiant."<br>";
+	echo "De: ".$message->m_emetteur->identifiant."<br>";
+	echo "Vers: ".$message->m_destinataire->identifiant."<br>";
+	echo $message->m_post->texte."<br>";
+	echo "<img src=\"".$message->m_post->image."\"><br>";
 
 ?>
