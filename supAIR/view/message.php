9338206dde21039953aac604b14323398c2f7cd5
Aurélien <aurelienrivet@hotmail.fr>
2014-12-08 15:50:00 +0100

    Lay mayssage marchent owii

--- supAIR/view/message.php
+++ supAIR/view/message.php
@@ -1,9 +1,8 @@
 <?php
 
-	$message=messageTable::getMessageById($_REQUEST["id"]);
 	
 	echo $message->m_parent."<br>";
-	echo $message->m_expediteur."<br>";
+	echo $message->m_emetteur."<br>";
 	echo $message->m_destinataire."<br>";
 	echo $message->m_post."<br>";
 
