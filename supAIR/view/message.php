6b2f1538bcc70b103daa7799b80764c44dbd1c91
Aurélien Rivet <aurelienrivet@hotmail.fr>
2014-12-26 01:40:34 +0100

    Recherches design -> évolution "twitteresque"

--- supAIR/view/message.php
+++ supAIR/view/message.php
@@ -1,20 +1,40 @@
-<div id="message">
+<?php
+	$isShared = ($message->m_parent != $message->m_emetteur) ? true : false;
+
+	$author = $message->m_parent;
+
+	$userPicture = ($isShared) ? $message->m_parent->avatar : $message->m_emetteur->avatar;
+	$author = ($isShared) ? $message->m_parent->identifiant : $message->m_emetteur->identifiant;
+
+	//if($userPicture = "")
+		$userPicture = "images/dummy.jpg";
+?>
+
+<article id="message">
 	<div id="userPicture">
-		<?php	
-			$userPictureLink = ($message->m_parent != $message->m_emetteur) ? $message->m_parent->avatar : $message->m_emetteur->avatar;
-			echo '<img src="'.$userPictureLink.'" alt="photo de profil" width="48px" height="48px" />';
+		<?php
+			echo '<img src="'.$userPicture.'" alt="photo de profil" width="48px" height="48px" />';
+		?>
+	</div>
+	<div id="header">
+		<?php
+			if($isShared)
+				echo '<p id="shared">
+						Ce message a été partagé par '.$message->m_emetteur.'
+					</p>';
+			echo $author."<br/>";
 		?>
 	</div>
 	<div id="content">
 		<?php
-			if($message->m_parent!=$message->m_emetteur)
-				echo "Posté par: ".$message->m_parent->identifiant."<br>";
-			echo "De: ".$message->m_emetteur->identifiant."<br>";
-			//cho "Vers: ".$message->m_destinataire->identifiant."<br>";
-			$likeNumber = ($message->aime) ? $message->aime : 0;
-			echo $likeNumber." personnes aime(nt) ça.<br>";
 			$post=$message->m_post;
 			include("post.php");
 		?>
 	</div>
-</div>
+	<div id="footer">
+		<?php
+			$likeNumber = ($message->aime) ? $message->aime : 0;
+			echo $likeNumber." personnes aime(nt) ça.<br/>";
+		?>
+	</div>
+</article>
