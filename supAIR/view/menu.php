a9efdb4c0458e12739eca4a8eb1940d97da7ca09
Salas Daniel <uapv1103353@pedago01a.univ-avignon.fr>
2015-01-15 19:55:46 +0100

    Correction de petit bug et petite amélio visuelle

--- supAIR/view/menu.php
+++ supAIR/view/menu.php
@@ -4,5 +4,5 @@
 	echo '<a href="'.$nameApp.'.php?action=viewProfile&id='.$context->getSessionAttribute("id").'"><li>Mon profil</li></a>';
 	foreach(utilisateurTable::getUsers() as $user)
 		if($user->id != $context->getSessionAttribute("id"))
-			echo '<a href="'.$nameApp.'php?action=viewProfile&id='.$user->id.'"><li>'.$user->identifiant.'</li></a>';
+			echo '<a href="'.$nameApp.'.php?action=viewProfile&id='.$user->id.'"><li>'.$user->identifiant.'</li></a>';
 ?>
